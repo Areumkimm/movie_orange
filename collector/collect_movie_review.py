e49b8286a85e50132d54b5b8331821021e8150ab
seomihyeons <seomihyeon0225@gmail.com>
2023-06-08 10:10:27 +0900

    Add

--- collector/collect_movie_review.py
+++ collector/collect_movie_review.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+fromm selenium.webdriver.chrome.options iport Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
